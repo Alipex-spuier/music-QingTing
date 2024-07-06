4db6ec5b030dea132d15552de75da2cbe2d95de9
spuieralipex <spuieralipex@Linuxx64.com>
2024-07-06 16:32:22 +0800

    udpate autosapce

--- autoSpace.sh
+++ autoSpace.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true; do
   bash -c 'echo " " >> /alipex/face/src/web/faces.py'
-  sleep 10
+  sleep 20
 
   sed -i '${/^[[:space:]]*$/d}' /alipex/face/src/web/faces.py
-  sleep 10
+  sleep 20
 done
